bd9e76ed9c9044521537d66fd017a21e98aa53c3
Juan Carrano <j.carrano@fu-berlin.de>
2018-05-07 13:29:15 +0200

    Make size of LoadF buffer configurable.

--- lauxlib.c
+++ lauxlib.c
@@ -635,7 +635,7 @@ LUALIB_API void luaL_unref (lua_State *L, int t, int ref) {
 typedef struct LoadF {
   int n;  /* number of pre-read characters */
   FILE *f;  /* file being read */
-  char buff[BUFSIZ];  /* area for reading file */
+  char buff[LUAL_BUFFERSIZE];  /* area for reading file */
 } LoadF;
 
 
